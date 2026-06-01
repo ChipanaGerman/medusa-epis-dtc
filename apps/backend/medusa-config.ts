57107a7e936d7966ca75aaef8c9fcc8bd2ebc122
ChipanaGerman <gchipanaj@unsa.edu.pe>
2026-05-31 19:15:19 -0500

    Configure Docker environment for Medusa

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -1,5 +1,7 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000"
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:9000,http://localhost:5173"
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
@@ -11,6 +13,24 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    },
+    databaseDriverOptions: {
+      ssl: false,
+      sslmode: "disable",
+    },
+    redisUrl: process.env.REDIS_URL,
+  },
+  admin: {
+    vite: (config) => {
+      return {
+        server: {
+          host: "0.0.0.0",
+          hmr: {
+            port: 5173,
+            clientPort: 5173,
+          },
+        },
+      }
+    },
+  },
 })
